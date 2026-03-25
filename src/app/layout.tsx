c8096393bac81dc44fcfd9761a862544849b1d57
mordellAdmin <kenny@mordell.app>
2026-03-25 12:40:40 +0300

    feat: Add Vercel Analytics and Speed Insights to the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter, Inter_Tight } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "@/context/CartContext";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -32,6 +34,8 @@ export default function RootLayout({
         <CartProvider>
           {children}
         </CartProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
